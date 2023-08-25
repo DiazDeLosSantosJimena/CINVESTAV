043a3376ec573bed60927131c4e31bc0a3c6d435
Eduardoh <al222111275@gmail.com>
2023-08-24 21:57:51 -0600

    correo

--- resources/views/mail/evaluado.blade.php
+++ resources/views/mail/evaluado.blade.php
@@ -60,10 +60,6 @@
 				display: none;
 			}
 
-			.mobile_hide {
-				display: none;
-			}
-
 			.row-content {
 				width: 100% !important;
 			}
@@ -160,7 +156,7 @@
 													<table class="image_block block-2" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
 															<td class="pad" style="padding-left:40px;padding-right:40px;width:100%;">
-																<div class="alignment" align="center" style="line-height:10px"><img class="fullWidth" src="https://f2edaf7f96.imgdist.com/public/users/Integrators/BeeProAgency/955344_939853/Online%20Review-cuate.png" style="height: auto; display: block; border: 0; max-width: 352px; width: 100%;" width="352" alt="I'm an image" title="I'm an image"></div>
+																<div class="alignment" align="center" style="line-height:10px"><img src="https://f2edaf7f96.imgdist.com/public/users/Integrators/BeeProAgency/955344_939853/Online%20Review-cuate.png" style="display: block; height: auto; border: 0; max-width: 256px; width: 100%;" width="256" alt="I'm an image" title="I'm an image"></div>
 															</td>
 														</tr>
 													</table>
@@ -196,13 +192,43 @@
 															<td class="pad" style="padding-bottom:10px;padding-left:40px;padding-right:40px;padding-top:10px;">
 																<div style="font-family: Arial, sans-serif">
 																	<div class style="font-size: 12px; font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; mso-line-height-alt: 18px; color: #555555; line-height: 1.5;">
-																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;">Esperamos que te encuentres bien. Nos complace informarte que el proyecto "{{$data['proyecto']}}", que presentaste ha sido evaluado con éxito. Ahora puedes revisar tu calificación en nuestra plataforma de evaluación.</span></p>
+																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;">Esperamos que usted se encuentre en buen estado. Nos complace comunicarle que el proyecto que usted presentó ha sido evaluado y aprobado exitosamente. En este momento, procederemos con la etapa correspondiente al pago, a fin de finalizar el proceso de registro de manera satisfactoria.</span></p>
 																	</div>
 																</div>
 															</td>
 														</tr>
 													</table>
-													<table class="divider_block block-2" width="100%" border="0" cellpadding="20" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+													<table class="text_block block-2" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad" style="padding-bottom:10px;padding-left:40px;padding-right:40px;padding-top:10px;">
+																<div style="font-family: Tahoma, Verdana, sans-serif">
+																	<div class style="font-size: 12px; font-family: 'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif; mso-line-height-alt: 18px; color: #555555; line-height: 1.5;">
+																		<p style="margin: 0; mso-line-height-alt: 27px;"><span style="font-size:18px;"><span style="background-color:transparent;font-size:18px;"><strong>Información</strong></span><strong style="font-family:'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif;font-family:'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif;font-size:18px;"><span style>&nbsp;de la cuenta bancaria.</span></strong></span></p>
+																		<p style="margin: 0; mso-line-height-alt: 22.5px;"><span style="font-size:15px;"><span style="font-size:15px;"><strong><strong>Nombre de Contacto</strong><strong>:</strong> </strong>Asociación Nacional por la Inclusividad Educativa en México.</span></span></p>
+																		<p style="margin: 0; mso-line-height-alt: 22.5px;"><span style="font-size:15px;"><span style="font-size:15px;"><strong>Correo de </strong></span><span style="font-size:15px;"><strong>facturación</strong></span><span style="font-size:15px;"><strong>:</strong></span></span><span style="font-size:15px;"> aniem.org@gmail.com</span><strong style="font-family:'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif;font-family:'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif;font-size:12px;"><br></strong><strong style="font-family:'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif;font-family:'Source Sans Pro', Tahoma, Verdana, Segoe, sans-serif;font-size:12px;"><span style="font-size:15px;">Cuenta bancaria BBVA: </span></strong><span style="font-size:15px;">0119923373</span></p>
+																		<p style="margin: 0; mso-line-height-alt: 22.5px;"><span style="font-size:15px;"><strong>Clabe Interbancaria: </strong>012180001199233734</span></p>
+																		<p style="margin: 0; mso-line-height-alt: 22.5px;"><span style="font-size:15px;"><strong>RFC</strong><strong>:</strong> ANI220117UC8</span></p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table class="text_block block-3" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad" style="padding-bottom:10px;padding-left:40px;padding-right:40px;padding-top:10px;">
+																<div style="font-family: Arial, sans-serif">
+																	<div class style="font-size: 12px; font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; mso-line-height-alt: 18px; color: #555555; line-height: 1.5;">
+																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;">Una vez que haya efectuado el pago, le solicitamos amablemente remitir el comprobante correspondiente al correo de facturación, adjuntando tanto su nombre como los siguientes datos:</span></p>
+																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;"><strong>Nombre del proyecto: &nbsp;</strong>{{$data['proyecto']}}</span></p>
+																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;"><strong>Numero de seguimiento: &nbsp;</strong>{{$data['np']}}</span></p>
+																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 18px;">&nbsp;</p>
+																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;">Te pedimos amablemente que ingreses en la plataforma y revises los nuevos apartados. Allí encontrarás información crucial que necesitamos que completes.</span></p>
+																	</div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table class="divider_block block-4" width="100%" border="0" cellpadding="20" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
 														<tr>
 															<td class="pad">
 																<div class="alignment" align="center">
@@ -211,19 +237,6 @@
 																			<td class="divider_inner" style="font-size: 1px; line-height: 1px; border-top: 0px solid #BBBBBB;"><span>&#8202;</span></td>
 																		</tr>
 																	</table>
-																</div>
-															</td>
-														</tr>
-													</table>
-													<table class="text_block block-4" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
-														<tr>
-															<td class="pad" style="padding-bottom:10px;padding-left:40px;padding-right:40px;padding-top:10px;">
-																<div style="font-family: Arial, sans-serif">
-																	<div class style="font-size: 12px; font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif; mso-line-height-alt: 18px; color: #555555; line-height: 1.5;">
-																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;">Una vez que haya efectuado el pago, le solicitamos amablemente remitir el comprobante correspondiente al correo de facturación, adjuntando tanto su nombre como el del proyecto en cuestión.</span></p>
-																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 18px;">&nbsp;</p>
-																		<p style="margin: 0; font-size: 12px; text-align: justify; mso-line-height-alt: 22.5px;"><span style="font-size:15px;">Le solicitamos amablemente que ingreses en la plataforma y revises los nuevos apartados. Allí encontrarás información crucial que necesitamos que completes.</span></p>
-																	</div>
 																</div>
 															</td>
 														</tr>
